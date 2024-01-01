8fdecff72e8e0ab36cedd3b5936288a4058cbd43
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-01-01 19:31:53 +0100

    Update runtime

--- ts/runtime.ts
+++ ts/runtime.ts
@@ -1,8 +1,9 @@
-import type { App } from "$types/app";
-import { AppRuntime } from "$ts/apps";
+import { AppRuntime } from "$ts/apps/runtime";
+import { Process } from "$ts/process";
+import type { App, AppMutator } from "$types/app";
 
 export class Runtime extends AppRuntime {
-  constructor(app: App) {
-    super(app);
+  constructor(app: App, mutator: AppMutator, process: Process) {
+    super(app, mutator, process);
   }
 }
